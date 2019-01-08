d7749e16cf484f3888d735206f42bea857880489
Taner Sener <tanersener@gmail.com>
2019-01-08 19:02:59 +0300

    update example tests

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_ffmpeg_example/example.dart';
+import 'package:flutter_ffmpeg_example/flutter_ffmpeg_example.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
