81b0da68ce2a5e0e83b161157fc58a8eb1c34d9d
Taner Sener <tanersener@gmail.com>
2019-10-28 00:12:16 +0000

    freeze v0.2.8

--- example/lib/flutter_ffmpeg_example.dart
+++ example/lib/flutter_ffmpeg_example.dart
@@ -171,8 +171,7 @@ class FlutterFFmpegTestAppState extends State<MainPage>
 
   void testRunCommand() {
     getLastReturnCode().then((rc) => print("Last rc: $rc"));
-    getLastCommandOutput().then((output) =>
-        debugPrint("Last command output: \"$output\"", wrapWidth: 1024));
+    getLastCommandOutput().then((output) => debugPrint("Last command output: \"$output\"", wrapWidth: 1024));
 
     print("Testing ParseArguments.");
 
@@ -305,9 +304,7 @@ class FlutterFFmpegTestAppState extends State<MainPage>
                     image3Path, fullVideoPath, ffmpegCodec, customOptions))
                 .then((rc) {
               if (rc == 0) {
-                getLastCommandOutput().then((output) => debugPrint(
-                    "Last command output: \"$output\"",
-                    wrapWidth: 1024));
+                getLastCommandOutput().then((output) => debugPrint("Last command output: \"$output\"", wrapWidth: 1024));
               }
             });
 
